dca52d519fc78f3e885a0d6004643005fdf4c6dc
Gholab <hajarelgholabzouri@gmail.com>
2025-09-28 22:41:11 +0200

    feat(frontend): bipper numpad #2

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -4,13 +4,16 @@ import { TitleComponent } from './ui/atoms/title/title.component';
 import { ButtonComponent } from './ui/atoms/button/button.component';
 import { NumberSelectorComponent } from './ui/atoms/number-selector/number-selector.component';
 import {CardItemComponent} from './ui/molecules/cardItem/cardItem.component';
+import {DigitButtonComponent} from "./ui/atoms/digit-button/digit-button.component";
+import { BipperComponent } from './ui/molecules/bipper/bipper.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TitleComponent, ButtonComponent, NumberSelectorComponent, CardItemComponent],
+  imports: [RouterOutlet, TitleComponent, ButtonComponent, NumberSelectorComponent, CardItemComponent, DigitButtonComponent, BipperComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('frontend');
 }
+  
