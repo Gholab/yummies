9bdbadd1d9b0e31020bfc262bb8719da34d54e1e
AmelieMuller <amelie17.muller@gmail.com>
2025-09-28 21:56:05 +0200

    #2 feat : numberSelector scss and tabs component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,15 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { TitleComponent } from './ui/atoms/title/title.component';
+import { ChangeDetectorRef, Component, ViewChild, TemplateRef, OnInit } from '@angular/core';
+import { TabsComponent, TabItem } from './ui/atoms/tabs/tabs.component';
 import { ButtonComponent } from './ui/atoms/button/button.component';
+import { TitleComponent } from './ui/atoms/title/title.component';
 import { NumberSelectorComponent } from './ui/atoms/number-selector/number-selector.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TitleComponent, ButtonComponent, NumberSelectorComponent],
+  standalone: true,
+  imports: [TabsComponent, ButtonComponent, TitleComponent, NumberSelectorComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class App implements OnInit {
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  @ViewChild('tplA', { static: true }) tplA!: TemplateRef<unknown>;
+  @ViewChild('tplB', { static: true }) tplB!: TemplateRef<unknown>;
+  @ViewChild('tplC', { static: true }) tplC!: TemplateRef<unknown>;
+
+  tabItems: TabItem[] = [];
+
+  ngOnInit(): void {
+    this.tabItems = [
+      { title: 'Aperçu',     template: this.tplA },
+      { title: 'Détails',    template: this.tplB },
+      { title: 'Paramètres', template: this.tplC, disabled: false },
+    ];
+    // pas besoin de detectChanges ici
+  }
 }
