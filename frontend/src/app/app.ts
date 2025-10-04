c3f7081c24b6e3734ee48654a46fe139086e0c7f
Gauthier MARTIN <45541503+martgauthier@users.noreply.github.com>
2025-10-04 16:53:52 +0200

    Feat/payment modals/9 (#10)
    * feat(payment_modal): ajout du code de base du payment modal
    
    * feat(payment_modal): added "division en parts égales" missing text
    
    * feat(payment_modal): added "Paiment un sur 5" missing text
    
    * feat(payment_modal): made payment modal more modulable
    
    * feat(payment_modal): removed background
    
    ---------
    
    Co-authored-by: CamilleAntonios <109613970+CamilleAntonios@users.noreply.github.com>

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -7,6 +7,8 @@ import { NumberSelectorComponent } from './ui/atoms/number-selector/number-selec
 import {CardItemComponent} from './ui/molecules/cardItem/cardItem.component';
 import {DigitButtonComponent} from "./ui/atoms/digit-button/digit-button.component";
 import { NumpadComponent } from './ui/molecules/numpad/numpad.component';
+import {PriceDisplayComponent} from './ui/atoms/price-display/price-display.component';
+import {PaymentModalComponent} from './ui/molecules/payment-modal/payment-modal.component';
 import {PaymentOptionComponent} from './ui/atoms/payment-option/payment-option.component';
 import {ChoosePaymentComponent} from './ui/molecules/choose-payment/choose-payment.component';
 import {PaymentStepsNavbarComponent} from './ui/molecules/payment-steps-navbar/payment-steps-navbar.component';
@@ -22,7 +24,8 @@ import {ModalComponent} from './ui/molecules/modal/modal.component';
             NumpadComponent, TabsComponent, 
             ModalComponent, EditItemModalComponent, 
             PaymentStepsNavbarComponent, ChoosePaymentComponent, 
-            PaymentOptionComponent],
+            PaymentOptionComponent, PaymentModalComponent, 
+            PriceDisplayComponent,],
   templateUrl: './app.html',
 })
 export class App implements OnInit {
--- frontend/src/app/ui/atoms/button/button.component.ts
+++ frontend/src/app/ui/atoms/button/button.component.ts
@@ -4,12 +4,13 @@ import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-button',
   imports: [NgClass],
+  standalone: true,
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
   @Input() text: string = '';
-  @Input() size: 'l' | 'm' | 's' = 'l';
+  @Input() size: 'fullwidth' | 'l' | 'm' | 's' = 'l';
   @Input() color: 'primary' | 'secondary' = 'primary';
   @Input() icon: string | null = null;
 }
