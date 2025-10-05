8b9001e5f32bd0162f72cf261962d8b7851209a0
AmelieMuller <amelie17.muller@gmail.com>
2025-10-05 19:08:57 +0200

    #32 feat : customized selection payment front done

--- frontend/src/app/ui/molecules/payment-item/payment-item.component.ts
+++ frontend/src/app/ui/molecules/payment-item/payment-item.component.ts
@@ -1,19 +1,34 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {CardItemComponent} from '../cardItem/cardItem.component';
 import {NumberSelectorComponent} from '../../atoms/number-selector/number-selector.component';
+import {CartItem} from '../../../models/cart-item-model';
 
 @Component({
-  selector: 'app-payment-item.component',
+  selector: 'app-payment-item',
   imports: [
     CardItemComponent,
     NumberSelectorComponent
   ],
   templateUrl: './payment-item.component.html',
   standalone: true,
-  styleUrl: './payment-item.component.scss'
+  styleUrl: './payment-item.component.scss',
+  host: { '[class.has-quantity]': 'chosenQuantity > 0'}
 })
 
 export class PaymentItem {
 
-  @Input() item: CartItem;
+  @Input() item!: CartItem ;
+  chosenQuantity = 0;
+  @Input() index! : number;
+  @Output() selectionChange = new EventEmitter<{ index: number; quantity: number }>();
+
+  getNumberRemaining(){
+    return this.item.howMany - this.chosenQuantity;
+  }
+
+  onQuantityChange(val: number) {
+    this.chosenQuantity = val;
+    this.selectionChange.emit({ index: this.index, quantity: val });
+  }
+
 }
--- frontend/src/app/ui/molecules/payment-steps-navbar/payment-steps-navbar.component.ts
+++ frontend/src/app/ui/molecules/payment-steps-navbar/payment-steps-navbar.component.ts
@@ -2,6 +2,7 @@ import {Component, Input} from '@angular/core';
 import {NumpadComponent} from '../numpad/numpad.component';
 import {TitleComponent} from '../../atoms/title/title.component';
 import {ChoosePaymentComponent} from '../choose-payment/choose-payment.component';
+import {CustomPayment} from '../custom-payment/custom-payment.component';
 
 type PaymentTab = {
   /** Titre affiché au-dessus de la nav bar pour l’étape courante */
@@ -12,7 +13,7 @@ type PaymentTab = {
 
 @Component({
   selector: 'app-payment-steps-navbar',
-  imports: [NumpadComponent, TitleComponent, ChoosePaymentComponent],
+  imports: [NumpadComponent, TitleComponent, ChoosePaymentComponent, CustomPayment],
   standalone: true,
   templateUrl: './payment-steps-navbar.component.html',
   styleUrl: './payment-steps-navbar.component.scss'
