d45baf1f074527b741ef1a06bde0e15ad696d7de
CamilleAntonios <camille.antonios@gmail.com>
2025-10-06 08:36:26 +0200

    #50 feat : added tables to database

--- backend/dining-service/src/shared/services/startup-logic.service.ts
+++ backend/dining-service/src/shared/services/startup-logic.service.ts
@@ -85,5 +85,145 @@ export class StartupLogicService implements OnApplicationBootstrap {
       await this.addTable(15);
     } catch (e) {
     }
+    try {
+      await this.addTable(16);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(17);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(18);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(19);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(20);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(21);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(22);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(23);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(24);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(25);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(26);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(27);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(28);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(29);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(30);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(31);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(32);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(33);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(34);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(35);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(36);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(37);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(38);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(39);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(40);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(41);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(42);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(43);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(44);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(45);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(46);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(47);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(48);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(49);
+    } catch (e) {
+    }
+    try {
+      await this.addTable(50);
+    } catch (e) {
+    }
   }
 }
