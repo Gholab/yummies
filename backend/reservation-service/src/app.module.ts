ab3d9471d88a591e7e434ca49cc036d4e92bdec1
CamilleAntonios <camille.antonios@gmail.com>
2025-10-30 23:45:03 +0100

    #93 Feat : Nest can connect to mongodb and POST /reservations and GET /reservations/:code work

--- backend/reservation-service/src/app.module.ts
+++ backend/reservation-service/src/app.module.ts
@@ -3,13 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {MongooseModule} from "@nestjs/mongoose";
 import {Reservation, ReservationSchema} from "./schemas/reservation.schema";
+import {ReservationController} from "./controllers/reservation.controller";
+import {ReservationService} from "./services/ReservationService";
 
 @Module({
   imports: [
-      MongooseModule.forRoot("mongodb://localhost:27021/yummies"),
+      MongooseModule.forRoot("mongodb://mongo_db_reservation:27017/yummies"),
       MongooseModule.forFeature([{name: Reservation.name, schema : ReservationSchema}])
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ReservationController],
+  providers: [AppService, ReservationService],
 })
 export class AppModule {}
