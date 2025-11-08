d3412af197b21953594393f2297e3ffd37b7c2c4
Gauthier MARTIN <45541503+martgauthier@users.noreply.github.com>
2025-11-08 16:24:33 +0100

    Feat/menu service reservation routes/94 (#110)
    * feat(reservation proxy): added basic reservations routes in menu service #94
    
    * feat(reservation proxy): matched camille format #94

--- backend/reservation-service/src/services/reservation.service.ts
+++ backend/reservation-service/src/services/reservation.service.ts
@@ -10,14 +10,17 @@ export class ReservationService {
 
     constructor(@InjectModel(Reservation.name) private reservationModel: Model<ReservationDocument>){}
 
-
     async create(reservation: Reservation) : Promise<Reservation> {
         const newRes = new this.reservationModel(reservation);
         return newRes.save();
     }
 
-    async findByCode(code: number): Promise<Reservation> {
-        let res = await this.reservationModel.findOne({code: code}).exec();
+    async findAll() {
+        return this.reservationModel.find().lean();
+    }
+
+    async findByCode(code: number) {
+        let res = await this.reservationModel.findOne({code: code}).lean();
         if(res === null){
             throw new NoReservationFoundErrorDto(code);
         }
