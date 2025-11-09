a0fefefc94fa4ce51b28ef0fe74ca59b3733016f
Gholab <120387865+Gholab@users.noreply.github.com>
2025-11-09 13:55:28 +0100

    Feat(reservation-service): calculate price companies #100 (#116)
    * feat(reservation-service): calculate price logic #100
    
    * feat(reservation-service): get orderCount from dining-service #100
    
    * feat(reservation-service): complete logic #100
    
    * feat(reservation-service): complete logic #100
    
    * feat(reservation-service): calculate price for reservations #100

--- backend/reservation-service/src/modules/reservation.module.ts
+++ backend/reservation-service/src/modules/reservation.module.ts
@@ -4,12 +4,16 @@ import { ReservationController } from '../controllers/reservation.controller';
 import { ReservationService } from '../services/reservation.service';
 import { StartupReservationService } from '../services/populate-reservations.service';
 import { Reservation, ReservationSchema } from '../schemas/reservation.schema';
+import { PaiementInfo, PaiementInfoSchema } from 'src/schemas/paiement-info.schema';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Reservation.name, schema: ReservationSchema },
+      { name: PaiementInfo.name, schema: PaiementInfoSchema },
     ]),
+    HttpModule,
   ],
   controllers: [ReservationController],
   providers: [ReservationService, StartupReservationService],
